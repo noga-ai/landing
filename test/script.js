bc4e4c7d115a7ea303b5b041346415ae6e5102b0
Daniel Schuldenfrei <schuldi@gmail.com>
2024-06-10 23:32:58 +0300

    test

---
+++ test/script.js
@@ -0,0 +1,47 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    new Typed('#subtitle', {
+        strings: ["We specialize in creating AI-powered data scientists. Our mission is to do it so well so that you can reach for loftier goals, so much faster."],
+        typeSpeed: 30,
+        backSpeed: 10,
+        showCursor: false,
+        startDelay: 0,
+        loop: false,
+    });
+
+    new Typed('#section2-text', {
+        strings: ["We don't believe in one-click AI wonders. By keeping data teams involved in the process, AI solutions create much more impact and value. We have designed our agents to empower users."],
+        typeSpeed: 30,
+        backSpeed: 10,
+        showCursor: false,
+        startDelay: 6000,
+        loop: false,
+    });
+
+    new Typed('#section3-text', {
+        strings: ["There is no one-agent-to-fit-all, that's why our technology allows integrations with company assets to achieve domain expertise. Yes, our agents can also scout the web if allowed."],
+        typeSpeed: 30,
+        backSpeed: 10,
+        showCursor: false,
+        startDelay: 14000,
+        loop: false,
+    });
+
+    new Typed('#section4-text', {
+        strings: ["Your data and requests are sensitive, that’s why we are building our own proprietary fine tuned LLM, ensuring that no data is sent to third parties."],
+        typeSpeed: 30,
+        backSpeed: 10,
+        showCursor: false,
+        startDelay: 22000,
+        loop: false,
+    });
+
+    new Typed('#section5-text', {
+        strings: ["Noga is in early access as we expand our capacity. To leverage Noga, contact us at <strong>info@getnoga.com</strong>"],
+        typeSpeed: 30,
+        backSpeed: 10,
+        showCursor: false,
+        startDelay: 29000,
+        loop: false,
+    });
+});
